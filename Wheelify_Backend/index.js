125daa7c0829b4e8bedf8fb78bac377551a3bfbe
Abhay-222 <abhaydasad21@gmail.com>
2025-06-12 12:54:17 +0530

    bug fix related to backend deployment

--- Wheelify_Backend/index.js
+++ Wheelify_Backend/index.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 3000;
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true ,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+}));
 
 
 //connect db
